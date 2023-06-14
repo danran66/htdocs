aaa7130564699a96c1791b5a105ce8dfc6b92372
danran66 <daniel2003scc@gmail.com>
2023-06-13 21:35:19 -0300

    teste

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php
+	define('HOST', 'sql9.freemysqlhosting.net');
+	define('USER', 'sql9624405');
+	define('PASS', 'ITA9MRP9rZ');
+	define('BASE', 'sql9624405');
+
+	$conn = new MySQLi(HOST,USER,PASS,BASE);
+	
+	
+
+?>
